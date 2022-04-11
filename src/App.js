da9c6f9e476072c29846a4eabf5efd54e85ac3b5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-11 22:36:59 +0600

    table design done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import UserTable from './components/UserTable/UserTable';
 
 function App() {
+  const fakedata=[{
+    id:1,name:"dulal",username:"something"
+  },
+  {id:2,name:"ponok"}
+]
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,6 +17,7 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable></UserTable>
         </div>
       </div>
     </div>
