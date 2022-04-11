4e5f4a95e18c709a8d23098add56b12735b6087b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-12 00:39:17 +0600

    form-handling done

--- src/components/AddUserForm/AddUserForm.js
+++ src/components/AddUserForm/AddUserForm.js
@@ -8,15 +8,16 @@ const AddUserForm = () => {
     const handleInput = (e)=>{
         const dt = e.target.value;
         setUser({...user,[e.target.name]:dt})
-        console.log("Google ")
+        // console.log("Google ")
     }
+    console.log(user);
     return (
         <div>
      <form>
       <label>Name</label>
-      <input type="text" onChange={handleInput} name="name" value="" />
+      <input type="text" onChange={handleInput} name="name" value={user.name} />
       <label>Username</label>
-      <input type="text" onChange={handleInput} name="username" value="" />
+      <input type="text" onChange={handleInput} name="username" value={user.username} />
       <button>Add new user</button>
     </form>
         </div>
