274febb989c6fa847622d2a58e5b2a690a189203
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-04-11 22:29:43 +0600

    fist view

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Crud operation</h1>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
     </div>
   );
 }
